fa1538da7d545e682a498ccdedfe4b3ce4f4b409
MR Muzi Nkambule <Muzinkambule789@gmail.com>
2025-04-25 19:11:22 +0200

    Edit from conflict-demo

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Edited in main branch.");
+        System.out.println("Edit from conflict-demo branch.");
+
 
     }
 }
