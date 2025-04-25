2acd30b302a2314988a2eb8d07f155a21592cb14
MR Muzi Nkambule <Muzinkambule789@gmail.com>
2025-04-25 19:19:51 +0200

    Resolved conflict

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Edit from conflict-demo branch.");
+        System.out.println("Resolved conflict manually.");
 
 
     }
